dd5e7d429ef3f898d013c13052d0c220e54d8c86
CVOrepin <orepin62@yandex.ru>
2025-01-29 11:20:19 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,14 @@
+from queue import PriorityQueue
+
+username = input ("имя пользователя: ")
+title = input ("заголовок заметки: ")
+content = input ("описание заметки: ")
+status = input("статус заметки: ")
+created_date = input("дата создания заметки Д-М-Г: ")
+issue_date = input("дата истечения заметки Д-М-Г: ")
+print(username,)
+print(title)
+print(content)
+print(status)
+print(created_date)
+print(issue_date)
