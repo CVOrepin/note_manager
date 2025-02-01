c144727f3aeeb7957215b82fcf91d7b8a250e6b3
CVOrepin <orepin62@yandex.ru>
2025-02-01 13:53:57 +0300

    Update date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -2,10 +2,10 @@ username = input ("имя пользователя: ")
 title = input ("заголовок заметки: ")
 content = input ("описание заметки: ")
 status = input("статус заметки: ")
-age_first = 2025
-age_end = 2026
 created_date = input("дата создания заметки Д-М: ")
 issue_date = input("дата истечения заметки Д-М: ")
+age_first = 2025
+age_end = 2026
 print(username,)
 print(title)
 print(content)
